e7d3902a78b255b2a339d3c1e8293030d425c86b
lewshubei <bei-02@graduate.utm.my>
2026-06-12 15:11:53 +0800

    change to https

--- api.js
+++ api.js
@@ -3,7 +3,7 @@
 // const API_BASE_URL = 'http://localhost/mycampus-cafe-slim-api/public/api';
 // Live backend API after deployment to cPanel/shared hosting
 // Inside your api.js file
-const API_BASE_URL = "http://mycampus10.atwebpages.com/api";
+const API_BASE_URL = "https://mycampus10.atwebpages.com/api"; // Change http → https
 function getToken() {
   return localStorage.getItem("mcafe_token");
 }
